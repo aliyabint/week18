aa5614925c47aa888b1ecb1bf69444b3032c1ee9
aliyabint <125250041+aliyabint@users.noreply.github.com>
2024-02-20 09:52:04 +0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,35 @@
 var cacheName = 'petstore-v1';
 var cacheFiles = [
     'index.html',
-    'product.js',
+    'products.js',
     'petstore.webmanifest',
     'images/catfood.jpeg',
-    'images/catfood2.png',
     'images/catfood2.png',
     'images/icon-store-512.png',
     'images/kittyfood.png'
 
 ];
- 
-self.addEventListener('install',(e) =>{
+
+self.addEventListener('install', (e) => {
     console.log('[Service Worker] Install');
     e.waitUntil(
-        caches.open(cacheName).then((cache)=>{
-            console.log ('[Service Worker] caching all the files');
+        caches.open(cacheName).then((cache) => {
+            console.log('[Service Worker] Caching all the files');
             return cache.addAll(cacheFiles);
 
         })
     );
 });
+
+self.addEventListener('fetch'
+    , function (e) {
+        e.respondWith(
+            // check if the cache has the file
+            caches.match(e.request).then(function (r) {
+                console.log('[Service Worker] Fetching resource: '
+                    + e.request.url);
+                // 'r' is the matching file if it exists in the cache
+                return r
+            })
+        );
+    });
